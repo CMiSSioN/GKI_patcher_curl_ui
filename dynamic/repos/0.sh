b25fb4a7646546916512c8c8b523de9783497b86
KMiSSioN <missionimp@mail.ru>
2025-12-24 21:55:34 +0300

    Update 0.sh

--- dynamic/repos/0.sh
+++ dynamic/repos/0.sh
@@ -1,4 +1,4 @@
-RELEASES_URL="https://api.github.com/repos/WildKernels/OnePlus_KernelSU_SUSFS/releases?per_page=1&page="
+RELEASES_URL="https://api.github.com/repos/WildKernels/OnePlus_KernelSU_SUSFS/releases?per_page=5"
 MODEL_RAW=$(getprop ro.product.model)
 log "$STR_DEVICE_MODEL: '$MODEL_RAW'"
 case "$MODEL_RAW" in
@@ -18,36 +18,80 @@ case "$MODEL_RAW" in
   CPH2645|CPH2647|CPH2691) SEARCH_KEYWORD="OP13r_" ;;
   CPH2723) SEARCH_KEYWORD="OP13S_" ;;
   CPH2621|PJF110) SEARCH_KEYWORD="OP-ACE-3V" ;;
-  "Armor 25T Pro") SEARCH_KEYWORD="OP-ACE-3V" ;;
+  "Armor 25T Pro") SEARCH_KEYWORD="OP12r_" ;;
   *) IS_ONEPLUS="0" ;;
 esac
 
 log "$STR_SEARCHING_KERNEL_PREFIX '$SEARCH_KEYWORD' $STR_SEARCHING_KERNEL_SUFFIX"
 
 log "$STR_KERNEL_VERSIONS_REPO_MESSAGE WildKernels/OnePlus_KernelSU_SUSFS"
-page=1
-while true; 
-do
-	log "$RELEASES_URL$page"
-	$CURL -s "$RELEASES_URL$page" > "$TMPDIR/releases.json"
-    
-	log "$STR_SEARCHING_KERNEL_PREFIX '$SEARCH_KEYWORD' $STR_SEARCHING_KERNEL_SUFFIX"
-	ARCHIVE_URL=$(grep -oE '"browser_download_url": *"[^"]*'"$SEARCH_KEYWORD"'[^"]*\.zip"' "$TMPDIR/releases.json" \
-	  | head -n 1 \
+variants_count=1
+exit_flag=0
+	
+$CURL -s "$RELEASES_URL" > "$TMPDIR/releases.json"
+
+log "$STR_SEARCHING_KERNEL_PREFIX '$SEARCH_KEYWORD' $STR_SEARCHING_KERNEL_SUFFIX"   
+counter=1
+variants=""
+while [ $counter -lt 6 ]; do
+  ARCHIVE_URL=$(grep -oE '"browser_download_url": *"[^"]*'"$SEARCH_KEYWORD"'[^"]*\.zip"' "$TMPDIR/releases.json" \
+	  | head -n $counter | tail -n 1 \
 	  | sed 's/.*"browser_download_url": *"\([^"]*\)".*/\1/')  
-	if [ -z "$ARCHIVE_URL" ]; then
-	  #log "❌ $STR_SEARCHING_KERNEL_FAIL $SEARCH_KEYWORD"
-	  #exit 0
-	  page=$((page + 1))
-	  continue;
-	fi
+  counter=$((counter + 1))
+  if [ -z $ARCHIVE_URL ]; then
+    break
+  else
+    variants="$variants $ARCHIVE_URL"
+  fi
+done
+
+if [ -z $variants ]; then
+  ARCHIVE_URL=''
+  log "❌ $STR_SEARCHING_KERNEL_FAIL $SEARCH_KEYWORD"
+else
+  variants="$variants Exit"
+  variants_count=1
+  log "Variants:"
+  for ARCHIVE_URL in $variants
+  do
+	log "$variants_count.$(basename "$ARCHIVE_URL")"
+	variants_count=$((variants_count + 1))
+  done
+  variants_count=$((variants_count - 1))
+  log "$variants_count"
+
+cat <<EOF
+VOL+ comply
+VOL- next
+EOF
+  
+  variant=1
+  while true; do
+    ARCHIVE_URL=$(echo -n ${variants} | cut -d' ' -f$variant)
 	log "$STR_SEARCHING_KERNEL_SUCCESS: $(basename "$ARCHIVE_URL")"
 	while true; do
 	  event="$(getevent -lqn -c1)"
 	  if echo "${event}" | grep -q "${volupkey}.*DOWN"; then
+		exit_flag=1
 		break
 	  elif echo "${event}" | grep -q "${voldownkey}.*DOWN"; then
-		exit 0
+		exit_flag=0
+		break
 	  fi
-	done	
-done
+	done
+	if [ $exit_flag -eq 0 ]; then
+		if [ $variant -eq $variants_count ]; 
+		then
+			variant=1
+		else
+			variant=$((variant + 1))
+		fi
+	else
+		break
+	fi
+  done
+  log "$variant"
+  if [ $variant -eq $variants_count ]; then
+     exit 0
+  fi
+fi
